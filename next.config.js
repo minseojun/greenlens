a2a060c9907fa586269c5c24689cfc692d2a2c57
minseojun <89567771+minseojun@users.noreply.github.com>
2026-05-15 13:18:25 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // pdf-parse가 필요로 하는 Node.js 내장 모듈 처리
+      config.externals = [...(config.externals || []), 'canvas', 'jsdom'];
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
